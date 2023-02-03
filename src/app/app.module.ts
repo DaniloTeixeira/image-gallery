bec1ca2a62677b76d3bd9652f2aa9cb523814e70
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-03 00:03:58 -0300

    Ajustando CSS e trocando loader de lugar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,6 +27,7 @@ import { AppComponent } from './app.component';
     CoreModule,
     SharedModule,
     HttpClientModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,11 +6,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SharedModule } from '../shared/shared.module';
 import { GalleryComponent } from './pages/gallery';
 import { HomeComponent } from './pages/home/home.component';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [GalleryComponent, HomeComponent],
-  imports: [CommonModule, ComponentsModule, SharedModule, MatSnackBarModule],
+  imports: [CommonModule, SharedModule, MatSnackBarModule],
   exports: [GalleryComponent],
 })
 export class CoreModule {}
