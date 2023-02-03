5e9d28c40446e9e9995f9d42d1e5160bc08f33bc
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2023-02-03 00:17:00 -0300

    Ajustes finais

--- src/app/core/services/loader/loader.service.ts
+++ src/app/core/services/loader/loader.service.ts
@@ -5,20 +5,41 @@ import { BehaviorSubject, map, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LoaderService {
+  /**
+   * Representa mensagem que será exibida no loader
+   */
   loaderText$ = new BehaviorSubject<string>('');
+
+  /**
+   * Define se loader deve ser mostrado na tela
+   */
   showLoader$!: Observable<boolean>;
 
+  /**
+   * Representa o loader aberto
+   */
   private openedLoader$ = new BehaviorSubject<string[]>([]);
 
   constructor() {
+    /**
+     * Define que loader só será exibido se existir algum loader aberto
+     */
     this.showLoader$ = this.openedLoader$.pipe(map((n) => n.length > 0));
   }
 
+  /**
+   * Mostra o loader na tela
+   *
+   * @param loaderText texto que será exibido no loader
+   */
   show(loaderText: string): void {
     this.loaderText$.next(loaderText);
     this.openedLoader$.next([...this.openedLoader$.getValue(), loaderText]);
   }
 
+  /**
+   * Esconde o loader na tela
+   */
   hide(): void {
     const [, ...loaders] = this.openedLoader$.getValue();
     this.openedLoader$.next(loaders);
